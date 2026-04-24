49883e51942b4940eab073d83adcc00e2f541beb
denisfetisov-git <denis.a.fetisov@gmail.com>
2026-04-24 15:42:02 +0200

    Update app - Generated by BESSER Web Editor

--- backend/pydantic_classes.py
+++ backend/pydantic_classes.py
@@ -12,8 +12,8 @@ from pydantic import BaseModel, field_validator
 # Classes are defined here
 ############################################
 class DocumentsCreate(BaseModel):
-    name: str
     ID: int
+    name: str
     date: date
 
 
